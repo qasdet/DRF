bc5273f0006b680640e32ad53a2fab103b682294
qasdet <qa-admin@sknt.ru>
2023-01-13 21:57:52 +0300

    less4

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="container">
+      <h1 className="mt-5">Users table</h1>
+      <p className="lead">Content got from Django REST Framework backend.</p>
+    </div>
+  )
+}
+
+export default Header
