2a8fb880d4d6fe55348e390c6866600a07dfb33d
qasdet <qa-admin@sknt.ru>
2023-01-21 23:28:29 +0300

    less6

--- userapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from userapp import models as userapp_models
-
-
-@admin.register(userapp_models.CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ["id", "username", "email", "is_active", "date_joined"]
-    ordering = ["-date_joined"]
--- userapp/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-
-from .models import CustomUser
-from .serializers import UserModelSerializer
-
-
-class UserModelViewSet(ModelViewSet):
-    serializer_class = UserModelSerializer
-    queryset = CustomUser.objects.all()
